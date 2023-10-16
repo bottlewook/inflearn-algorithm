cfd64bfc174d941a0002ff3181e9b96c4886856f
wookki <bwlimtony@gmail.com>
2023-10-17 02:56:11 +0900

    feature/section-1/대문자로통일

---
+++ section1/대문자로통일.js
@@ -0,0 +1,13 @@
+function solution(s) {
+  let answer = "";
+  for (let x of s) {
+    let num = x.charCodeAt(0);
+    if (num >= 97 && num <= 122) answer += String.fromCharCode(num - 32);
+    else answer += x;
+  }
+
+  return answer;
+}
+
+let str = "ItisTimeToStudy";
+console.log(solution(str));
