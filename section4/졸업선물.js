472d303387e6d28941a268e8edaa735ba81cb1fb
wookki <bwlimtony@gmail.com>
2023-10-18 23:19:43 +0900

    feature/section-4/졸업선물

--- section4/졸업선물.js
+++ section4/졸업선물.js
@@ -1,27 +1,26 @@
-function solution(test) {
+function solution(m, product) {
   let answer = 0;
-  m = test.length;
-  n = test[0].length;
-  for (let i = 1; i <= n; i++) {
-    for (let j = 1; j <= n; j++) {
-      let cnt = 0;
-      for (let k = 0; k < m; k++) {
-        let pi = (pj = 0);
-        for (let s = 0; s < n; s++) {
-          if (test[k][s] === i) pi = s;
-          if (test[k][s] === j) pj = s;
-        }
-        if (pi < pj) cnt++;
+  product.sort((a, b) => a[0] + a[1] - (b[0] + b[1]));
+  for (let i = 0; i < product.length; i++) {
+    let money = m - (product[i][0] / 2 + product[i][1]);
+    let count = 1;
+    for (let j = 0; j < product.length; j++) {
+      if (i !== j && product[j][0] + product[j][1] <= money) {
+        money -= product[j][0] + product[j][1];
+        count++;
       }
-      if (cnt === m) answer++;
     }
+    answer = Math.max(answer, count);
   }
+
   return answer;
 }
 
 let arr = [
-  [3, 4, 1, 2],
-  [4, 3, 2, 1],
-  [3, 1, 4, 2],
+  [6, 6],
+  [2, 2],
+  [4, 3],
+  [4, 5],
+  [10, 3],
 ];
-console.log(solution(arr));
+console.log(solution(28, arr));
