8eb8b070e7a761c5f4f30f22939eab6df0e55758
wookki <bwlimtony@gmail.com>
2023-10-23 23:27:47 +0900

    feature/section-7/삽입정렬

---
+++ section7/삽입정렬.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentVal = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentVal;
+  }
+  return arr;
+}
+
+let arr = [11, 7, 5, 6, 10, 9];
+console.log(solution(arr));
