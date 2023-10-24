1b1021c95e4e180fb15d7be7151192643e0537c6
wookki <bwlimtony@gmail.com>
2023-10-24 23:14:38 +0900

    feature/section-7/마구간정하기

---
+++ section7/마구간정하기.js
@@ -0,0 +1,32 @@
+function count(stable, dist) {
+  let count = 1;
+  let position = stable[0];
+  for (let i = 1; i < stable.length; i++) {
+    if (stable[i] - position >= dist) {
+      count++;
+      position = stable[i];
+    }
+  }
+  return count;
+}
+function solution(c, stable) {
+  let answer = 0;
+  stable.sort((a, b) => a - b);
+  let lt = 1;
+  let rt = stable[stable.length - 1];
+
+  while (lt <= rt) {
+    let mid = Math.floor((lt + rt) / 2);
+    if (count(stable, mid) >= c) {
+      answer = mid;
+      lt = mid + 1;
+    } else {
+      rt = mid - 1;
+    }
+  }
+
+  return answer;
+}
+
+let arr = [1, 2, 8, 4, 9];
+console.log(solution(3, arr));
