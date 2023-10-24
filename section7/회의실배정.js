0e91b440d25f256d4a108113ff73761002f606a9
wookki <bwlimtony@gmail.com>
2023-10-24 15:43:16 +0900

    feature/section-7/회의실배정

---
+++ section7/회의실배정.js
@@ -0,0 +1,22 @@
+function solution(meeting) {
+  let answer = 0;
+  meeting.sort((a, b) => {
+    if (a[1] === b[1]) return a[0] - b[0];
+    else return a[1] - b[1];
+  });
+  let meetingTime = 0;
+
+  for (let x of meeting) {
+    if (meetingTime <= x[0]) meetingTime = x[1];
+    answer++;
+  }
+}
+
+let arr = [
+  [1, 4],
+  [2, 3],
+  [3, 5],
+  [4, 6],
+  [5, 7],
+];
+console.log(solution(arr));
