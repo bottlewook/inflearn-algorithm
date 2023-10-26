4d1077a8b58a8014d4af995fae742e112b99fcec
wookki <bwlimtony@gmail.com>
2023-10-26 23:01:09 +0900

    feature/section-8/중복순열

---
+++ section8/중복순열.js
@@ -0,0 +1,19 @@
+function solution(n, m) {
+  let answer = [];
+  let checkList = Array.from({ length: m }, () => 0);
+
+  function DFS(level) {
+    if (level === m) {
+      answer.push(checkList);
+    } else {
+      for (let i = 1; i <= n; i++) {
+        checkList[level] = i;
+        DFS(level + 1);
+      }
+    }
+  }
+  DFS(0);
+  return answer;
+}
+
+console.log(solution(3, 2));
