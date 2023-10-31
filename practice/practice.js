7559c72a933375e5addf0a85d70f3d6ce4cacd04
wookki <bwlimtony@gmail.com>
2023-11-01 05:26:56 +0900

    feature/section-10/문자열압축

--- practice/practice.js
+++ practice/practice.js
@@ -1,20 +1,22 @@
-function solution(s) {
-  let answer = "";
-  let count = 1;
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === s[i + 1]) {
-      count++;
-    } else {
-      if (count === 1) {
-        answer += s[i];
-      } else {
-        answer += s[i] + count.toString();
-      }
-      count = 1;
+function solution(n, arr) {
+  let answer = 0;
+  let max = Number.MIN_SAFE_INTEGER;
+  for (let x of arr) {
+    let temp = x;
+    let sum = 0;
+    while (temp >= 1) {
+      sum += temp % 10;
+      temp = Math.floor(temp / 10);
+    }
+    if (sum > max) {
+      max = sum;
+      answer = x;
+    } else if (max === sum) {
+      if (x > answer) answer = x;
     }
   }
   return answer;
 }
 
-let str = "KKHSSSSSSSE";
-console.log(solution(str));
+let arr = [128, 460, 603, 40, 521, 137, 123];
+console.log(solution(7, arr));
