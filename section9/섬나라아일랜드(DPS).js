78219686ffed362bf7211840ecf8ad385013d02c
wookki <bwlimtony@gmail.com>
2023-10-31 11:40:18 +0900

    feature/section-9/섬나라아일랜드(DPS)

---
+++ section9/섬나라아일랜드(DPS).js
@@ -0,0 +1,46 @@
+function solution(board) {
+  let answer = 0;
+  let length = board.length;
+  let dx = [-1, -1, 0, 1, 1, 1, 0, -1];
+  let dy = [0, 1, 1, 1, 0, -1, -1, -1];
+
+  function DFS(x, y) {
+    board[x][y] = 0;
+    for (let k = 0; k < dx.length; k++) {
+      let nx = x + dx[k];
+      let ny = y + dy[k];
+      if (
+        nx >= 0 &&
+        nx < length &&
+        ny >= 0 &&
+        ny < length &&
+        board[nx][ny] === 1
+      ) {
+        DFS(nx, ny);
+      }
+    }
+  }
+
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+      if (board[i][j] === 1) {
+        answer++;
+        DFS(i, j);
+      }
+    }
+  }
+
+  return answer;
+}
+
+let arr = [
+  [1, 1, 0, 0, 0, 1, 0],
+  [0, 1, 1, 0, 1, 1, 0],
+  [0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 1, 1],
+  [1, 1, 0, 1, 1, 0, 0],
+  [1, 0, 0, 0, 1, 0, 0],
+  [1, 0, 1, 0, 1, 0, 0],
+];
+
+console.log(solution(arr));
