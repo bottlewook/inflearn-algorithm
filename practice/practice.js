512925389e5c431cb6394a4fd5ee53460e6b79b7
wookki <bwlimtony@gmail.com>
2023-11-01 01:38:55 +0900

    feature/section-10/봉우리

--- practice/practice.js
+++ practice/practice.js
@@ -1,16 +1,37 @@
 function solution(arr) {
-  let answer = [];
+  let answer = 0;
+  let dx = [-1, 0, 1, 0];
+  let dy = [0, 1, 0, -1];
+
   for (let i = 0; i < arr.length; i++) {
-    let rank = 1;
     for (let j = 0; j < arr.length; j++) {
-      if (arr[i] < arr[j] && i !== j) {
-        rank++;
+      let flag = 1;
+      for (let k = 0; k < dx.length; k++) {
+        let nx = i + dx[k];
+        let ny = j + dy[k];
+        let top = arr[i][j];
+        if (
+          nx >= 0 &&
+          nx < arr.length &&
+          ny >= 0 &&
+          ny < arr.length &&
+          arr[nx][ny] >= top
+        ) {
+          flag = 0;
+          break;
+        }
       }
+      if (flag) answer++;
     }
-    answer[i] = rank;
   }
   return answer;
 }
 
-let arr = [87, 87, 92, 100, 76];
+let arr = [
+  [5, 3, 7, 2, 3],
+  [3, 7, 1, 6, 1],
+  [7, 2, 5, 3, 4],
+  [4, 3, 6, 4, 1],
+  [8, 7, 3, 5, 2],
+];
 console.log(solution(arr));
