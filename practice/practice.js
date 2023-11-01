408663d543bd8cd99d56ab2f4e47d57b2b19afd4
wookki <bwlimtony@gmail.com>
2023-11-01 14:31:31 +0900

    feature/section-10/최대매출

--- practice/practice.js
+++ practice/practice.js
@@ -1,16 +1,17 @@
-function solution(m, arr) {
+function solution(k, arr) {
   let answer = 0;
   let sum = 0;
-  let lt = 0;
-  for (let rt = 0; rt < arr.length; rt++) {
-    sum += arr[rt];
-    while (sum > m) {
-      sum -= arr[lt++];
-    }
-    answer += rt - lt + 1;
+  for (let i = 0; i < k; i++) {
+    sum += arr[i];
   }
+  answer = sum;
+  for (let i = k; i < arr.length; i++) {
+    sum += arr[i] - arr[i - k];
+    answer = Math.max(answer, sum);
+  }
+
   return answer;
 }
 
-let a = [1, 3, 1, 2, 3];
-console.log(solution(5, a));
+let a = [12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
+console.log(solution(3, a));
