cc30d9c1795ce98ff9413dd862cc47c55905a378
wookki <bwlimtony@gmail.com>
2023-11-01 11:37:14 +0900

    feature/section-10/졸업선물

--- practice/practice.js
+++ practice/practice.js
@@ -1,29 +1,30 @@
-function solution(test) {
+function solution(m, product) {
   let answer = 0;
-  let m = test.length; // 3
-  let n = test[0].length; // 4
-
-  for (let i = 1; i <= n; i++) {
-    for (let j = 0; j <= n; j++) {
-      let count = 0;
-      for (let k = 0; k < m; k++) {
-        let pi = 0;
-        let pj = 0;
-        for (let s = 0; s < n; s++) {
-          if (test[k][s] === i) pi = s;
-          if (test[k][s] === j) pj = s;
-        }
-        if (pi < pj) count++;
+  product.sort((a, b) => {
+    if (a[0] === b[0]) return a[1] - b[1];
+    else return a[0] - b[0];
+  });
+  console.log(product);
+  for (let i = 0; i < product.length; i++) {
+    let max = 1;
+    let money = m - (product[i][0] / 2 + product[i][1]);
+    for (let j = 0; j < product.length; j++) {
+      if (i !== j && product[j][0] + product[j][1] > money) break;
+      if (i !== j && product[j][0] + product[j][1] <= money) {
+        max++;
+        money -= product[j][0] + product[j][1];
       }
-      if (count === m) answer++;
     }
+    answer = Math.max(answer, max);
   }
   return answer;
 }
 
 let arr = [
-  [3, 4, 1, 2],
-  [4, 3, 2, 1],
-  [3, 1, 4, 2],
+  [6, 6],
+  [2, 2],
+  [4, 3],
+  [4, 5],
+  [10, 3],
 ];
-console.log(solution(arr));
+console.log(solution(28, arr));
