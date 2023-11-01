54b6c4311f04c407391b33818c65c391a9375ff5
wookki <bwlimtony@gmail.com>
2023-11-01 14:40:35 +0900

    feature/section-10/올바른 괄호

--- practice/practice.js
+++ practice/practice.js
@@ -1,17 +1,17 @@
-function solution(k, arr) {
-  let answer = 0;
-  let sum = 0;
-  for (let i = 0; i < k; i++) {
-    sum += arr[i];
-  }
-  answer = sum;
-  for (let i = k; i < arr.length; i++) {
-    sum += arr[i] - arr[i - k];
-    answer = Math.max(answer, sum);
+function solution(s) {
+  let stack = [];
+  for (let x of s) {
+    if (x === "(") stack.push("(");
+    else {
+      let temp = stack.pop();
+      if (temp === undefined) return "NO";
+    }
   }
 
-  return answer;
+  if (stack.length) return "NO";
+
+  return "YES";
 }
 
-let a = [12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
-console.log(solution(3, a));
+let a = "(()(()))(()";
+console.log(solution(a));
