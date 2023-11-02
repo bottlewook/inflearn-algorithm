6b741cb10635be14071265e3f8a7ac79edbbc74d
wookki <bwlimtony@gmail.com>
2023-11-02 11:12:08 +0900

    feature/section-10/LRU

--- practice/practice.js
+++ practice/practice.js
@@ -1,13 +1,16 @@
-function solution(arr) {
-  for (let i = arr.length - 1; i >= 0; i--) {
-    for (let j = 0; j <= i - 1; j++) {
-      if (arr[j] > 0 && arr[j + 1] < 0) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-      }
+function solution(size, arr) {
+  let answer = Array.from({ length: size }, () => 0);
+  for (let i = 0; i < arr.length; i++) {
+    if (!answer.includes(arr[i])) {
+      answer.unshift(arr[i]);
+      answer.pop();
+    } else {
+      answer.splice(answer.indexOf(arr[i]), 1);
+      answer.unshift(arr[i]);
     }
   }
-  return arr;
+  return answer;
 }
 
-let arr = [1, 2, 3, -3, -2, 5, 6, -6];
-console.log(solution(arr));
+let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7];
+console.log(solution(5, arr));
