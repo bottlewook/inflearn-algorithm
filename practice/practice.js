e08a3cb07cdb677cb8515850c9dbaa9abd621ba3
wookki <bwlimtony@gmail.com>
2023-11-02 11:59:33 +0900

    feature/section-10/결혼식

--- practice/practice.js
+++ practice/practice.js
@@ -1,24 +1,32 @@
-function solution(meeting) {
-  let answer = 0;
-  meeting.sort((a, b) => {
-    if (a[1] === b[1]) return a[0] - b[0];
-    else return a[1] - b[1];
+function solution(times) {
+  let answer = Number.MIN_SAFE_INTEGER;
+  let table = [];
+  for (let time of times) {
+    table.push([time[0], "s"]);
+    table.push([time[1], "e"]);
+  }
+  table.sort((a, b) => {
+    if (a[0] === b[0]) return a[1].charCodeAt(0) - b[1].charCodeAt(0);
+    else return a[0] - b[0];
   });
-  let endTime = 0;
-  for (let x of meeting) {
-    if (x[0] >= endTime) {
-      answer++;
-      endTime = x[1];
+
+  let people = 0;
+  for (let x of table) {
+    if (x[1] === "s") {
+      people++;
+    } else {
+      people--;
     }
+    answer = Math.max(answer, people);
   }
   return answer;
 }
 
 let arr = [
-  [1, 4],
-  [2, 3],
-  [3, 5],
-  [4, 6],
-  [5, 7],
+  [14, 18],
+  [12, 15],
+  [15, 20],
+  [20, 30],
+  [5, 14],
 ];
 console.log(solution(arr));
