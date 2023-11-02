21cecaffea10e6fd188a9aa5179eb994457356fd
wookki <bwlimtony@gmail.com>
2023-11-02 10:49:28 +0900

    feature/section-10/삽입정렬

--- practice/practice.js
+++ practice/practice.js
@@ -1,13 +1,10 @@
 function solution(arr) {
-  for (let i = arr.length - 1; i >= 0; i--) {
-    let noSwap = true;
-    for (let j = 0; j < i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-        noSwap = false;
-      }
+  for (let i = 1; i < arr.length; i++) {
+    let currentVal = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
     }
-    if (noSwap) break;
+    arr[j + 1] = currentVal;
   }
   return arr;
 }
