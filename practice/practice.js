212ca3c2316638f826aba3185915e43f3e3f1873
wookki <bwlimtony@gmail.com>
2023-11-03 11:15:26 +0900

    feature/section-10/부분집합구하기

--- practice/practice.js
+++ practice/practice.js
@@ -1,13 +1,24 @@
 function solution(n) {
-  function DFS(n) {
-    if (n > 7) return;
-    else {
-      DFS(2 * n);
-      DFS(2 * n + 1);
-      console.log(n);
+  let answer = [];
+  let checkList = Array.from({ length: n }, () => 0);
+
+  function DFS(vertex) {
+    if (vertex === n + 1) {
+      let temp = "";
+      for (let i = 1; i <= checkList.length; i++) {
+        if (checkList[i]) temp += i + " ";
+      }
+      if (temp.trim().length > 0) answer.push(temp);
+    } else {
+      checkList[vertex] = 1;
+      DFS(vertex + 1);
+      checkList[vertex] = 0;
+      DFS(vertex + 1);
     }
   }
-  DFS(n);
+
+  DFS(1);
+  return answer;
 }
 
-console.log(solution(1));
+console.log(solution(3));
