b65c7a4a93eb2d31277f6e51b65f2900e32302a2
wookki <bwlimtony@gmail.com>
2023-11-03 21:41:33 +0900

    feature/section-10/동전교환

--- practice/practice.js
+++ practice/practice.js
@@ -1,19 +1,19 @@
-function solution(n, m) {
-  let answer = [];
-  let checkList = Array.from({ length: m }, () => 0);
-  function DFS(level) {
-    if (level === m) {
-      answer.push([...checkList]);
+function solution(m, arr) {
+  let answer = Number.MAX_SAFE_INTEGER;
+  function DFS(level, sum) {
+    if (sum > m) return;
+    if (sum === m) {
+      answer = Math.min(answer, level);
     } else {
-      for (let i = 1; i <= n; i++) {
-        checkList[level] = i;
-        DFS(level + 1);
+      for (let i = 0; i < arr.length; i++) {
+        DFS(level + 1, sum + arr[i]);
       }
     }
   }
-  DFS(0);
+  DFS(0, 0);
 
   return answer;
 }
 
-console.log(solution(3, 2));
+let arr = [1, 2, 5];
+console.log(solution(15, arr));
