e7363fec76b2542f0e07ee63fa89dcd04bb6631a
wookki <bwlimtony@gmail.com>
2023-11-03 22:21:01 +0900

    feature/section-10/조합의경우수

--- practice/practice.js
+++ practice/practice.js
@@ -1,6 +1,17 @@
-function solution(n) {
-  if (n === 1) return 1;
-  return n * solution(n - 1);
+function solution(n, r) {
+  let answer;
+  let memo = Array.from(Array(50), () => Array(50).fill(0));
+
+  function DFS(n, r) {
+    if (memo[n][r]) return memo[n][r];
+    if (n === r || r === 0) {
+      return 1;
+    } else {
+      return (memo[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r));
+    }
+  }
+  answer = DFS(n, r);
+  return answer;
 }
 
-console.log(solution(5));
+console.log(solution(49, 10));
