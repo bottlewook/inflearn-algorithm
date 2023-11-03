ee9d6957e772c3bc7578fc12133d4ac080f45702
wookki <bwlimtony@gmail.com>
2023-11-03 10:22:28 +0900

    feature/section-10/재귀

--- practice/practice.js
+++ practice/practice.js
@@ -1,32 +1,12 @@
-function solution(times) {
-  let answer = Number.MIN_SAFE_INTEGER;
-  let table = [];
-  for (let time of times) {
-    table.push([time[0], "s"]);
-    table.push([time[1], "e"]);
-  }
-  table.sort((a, b) => {
-    if (a[0] === b[0]) return a[1].charCodeAt(0) - b[1].charCodeAt(0);
-    else return a[0] - b[0];
-  });
-
-  let people = 0;
-  for (let x of table) {
-    if (x[1] === "s") {
-      people++;
-    } else {
-      people--;
+function solution(n) {
+  function DFS(n) {
+    if (n < 1) return;
+    else {
+      DFS(n - 1);
+      console.log(n);
     }
-    answer = Math.max(answer, people);
   }
-  return answer;
+  DFS(n);
 }
 
-let arr = [
-  [14, 18],
-  [12, 15],
-  [15, 20],
-  [20, 30],
-  [5, 14],
-];
-console.log(solution(arr));
+console.log(solution(3));
