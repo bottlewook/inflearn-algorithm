ea171438dd96c5cd8a616be2408b0035dc7a974e
wookki <bwlimtony@gmail.com>
2023-11-04 12:06:30 +0900

    feature/section-9/송아지찾기

--- practice/practice.js
+++ practice/practice.js
@@ -1,18 +1,24 @@
-function solution() {
-  let answer = [];
+function solution(s, e) {
+  let answer = 0;
+  let checkList = Array.from({ length: 10001 }, () => 0);
+  let distance = Array.from({ length: 10001 }, () => 0);
   let queue = [];
-
-  queue.push(1);
+  queue.push(s);
+  checkList[s] = 1;
+  distance[s] = 0;
   while (queue.length) {
     let value = queue.shift();
-    answer.push(value);
-    for (let nextValue of [value * 2, value * 2 + 1]) {
-      if (nextValue > 7) continue;
-      queue.push(nextValue);
+    for (let nextValue of [value - 1, value + 1, value + 5]) {
+      if (nextValue === e) return (answer = distance[value] + 1);
+      if (nextValue > 0 && nextValue <= 10000 && checkList[nextValue] === 0) {
+        checkList[nextValue] = 1;
+        queue.push(nextValue);
+        distance[nextValue] = distance[value] + 1;
+      }
     }
   }
 
   return answer;
 }
 
-console.log(solution());
+console.log(solution(8, 3));
