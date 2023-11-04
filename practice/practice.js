10a19767d9a360eb0e6891c373aff35dab13a7a6
wookki <bwlimtony@gmail.com>
2023-11-04 11:10:53 +0900

    feature/section-9/인접행렬

--- practice/practice.js
+++ practice/practice.js
@@ -1,17 +1,39 @@
-function solution(n, k, arr, m) {
+function solution(n, arr) {
   let answer = 0;
-  function DFS(level, s, sum) {
-    if (level === k) {
-      if (sum % m === 0) answer++;
-    } else {
-      for (let i = s; i < n; i++) {
-        DFS(level + 1, i + 1, sum + arr[i]);
+  let graph = Array.from(Array(n + 1), () => Array(n + 1).fill(0));
+  let checkList = Array.from({ length: n + 1 }, () => 0);
+
+  for ([x, y] of arr) {
+    graph[x][y] = 1;
+  }
+
+  function DFS(vertex) {
+    if (vertex === 5) answer++;
+    else {
+      for (let i = 1; i <= n; i++) {
+        if (graph[vertex][i] === 1 && checkList[i] === 0) {
+          checkList[i] = 1;
+          DFS(i);
+          checkList[i] = 0;
+        }
       }
     }
   }
-  DFS(0, 0, 0);
+  checkList[1] = 1;
+  DFS(1);
+
   return answer;
 }
 
-let arr = [2, 4, 5, 8, 12];
-console.log(solution(5, 3, arr, 6));
+let arr = [
+  [1, 2],
+  [1, 3],
+  [1, 4],
+  [2, 1],
+  [2, 3],
+  [2, 5],
+  [3, 4],
+  [4, 2],
+  [4, 5],
+];
+console.log(solution(5, arr));
