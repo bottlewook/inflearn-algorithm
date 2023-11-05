5855566c3fb6d5daec94a23fbec8486c1c8ea7be
wookki <bwlimtony@gmail.com>
2023-11-05 10:01:20 +0900

    feature/section-10/돌다리건너기

--- practice/practice.js
+++ practice/practice.js
@@ -1,12 +1,12 @@
 function solution(n) {
-  let checkList = Array.from({ length: n + 1 }, () => 0);
+  let checkList = Array.from({ length: n + 2 }, () => 0);
   checkList[1] = 1;
   checkList[2] = 2;
 
-  for (let i = 3; i <= n; i++) {
+  for (let i = 3; i <= n + 1; i++) {
     checkList[i] = checkList[i - 1] + checkList[i - 2];
   }
-  return checkList[n];
+  return checkList[n + 1];
 }
 
 console.log(solution(7));
