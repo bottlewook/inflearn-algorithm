1606fdcf8fc4bc86ac787cf5e35b42127037d5cd
wookki <bwlimtony@gmail.com>
2023-11-05 09:59:46 +0900

    feature/section-10/계단오르기

--- practice/practice.js
+++ practice/practice.js
@@ -1,41 +1,12 @@
-function solution(board) {
-  let answer = 0;
-  let n = board.length;
-  let dx = [-1, -1, 0, 1, 1, 1, 0, -1];
-  let dy = [0, 1, 1, 1, 0, -1, -1, -1];
+function solution(n) {
+  let checkList = Array.from({ length: n + 1 }, () => 0);
+  checkList[1] = 1;
+  checkList[2] = 2;
 
-  function DFS(x, y) {
-    board[x][y] = 0;
-    for (let k = 0; k < dx.length; k++) {
-      let nx = x + dx[k];
-      let ny = y + dy[k];
-      if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] === 1) {
-        DFS(nx, ny);
-      }
-    }
+  for (let i = 3; i <= n; i++) {
+    checkList[i] = checkList[i - 1] + checkList[i - 2];
   }
-
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      if (board[i][j] === 1) {
-        board[i][j] = 0;
-        answer++;
-        DFS(i, j);
-      }
-    }
-  }
-
-  return answer;
+  return checkList[n];
 }
 
-let arr = [
-  [1, 1, 0, 0, 0, 1, 0],
-  [0, 1, 1, 0, 1, 1, 0],
-  [0, 1, 0, 0, 0, 0, 0],
-  [0, 0, 0, 1, 0, 1, 1],
-  [1, 1, 0, 1, 1, 0, 0],
-  [1, 0, 0, 0, 1, 0, 0],
-  [1, 0, 1, 0, 1, 0, 0],
-];
-
-console.log(solution(arr));
+console.log(solution(7));
