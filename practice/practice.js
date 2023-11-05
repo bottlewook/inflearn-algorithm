0bccc35a4e503eb73c50c3666d08119c7a2f145d
wookki <bwlimtony@gmail.com>
2023-11-05 09:36:02 +0900

    feature/section-9/섬나라아일랜드

--- practice/practice.js
+++ practice/practice.js
@@ -1,24 +1,46 @@
-function solution(s, e) {
+function solution(board) {
   let answer = 0;
-  let checkList = Array.from({ length: 10001 }, () => 0);
-  let distance = Array.from({ length: 10001 }, () => 0);
+  let dx = [-1, -1, 0, 1, 1, 1, 0, -1];
+  let dy = [0, 1, 1, 1, 0, -1, -1, -1];
   let queue = [];
-  queue.push(s);
-  checkList[s] = 1;
-  distance[s] = 0;
-  while (queue.length) {
-    let value = queue.shift();
-    for (let nextValue of [value - 1, value + 1, value + 5]) {
-      if (nextValue === e) return (answer = distance[value] + 1);
-      if (nextValue > 0 && nextValue <= 10000 && checkList[nextValue] === 0) {
-        checkList[nextValue] = 1;
-        queue.push(nextValue);
-        distance[nextValue] = distance[value] + 1;
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board.length; j++) {
+      if (board[i][j] === 1) {
+        board[i][j] = 0;
+        queue.push([i, j]);
+        answer++;
+        while (queue.length) {
+          let [x, y] = queue.shift();
+          for (let k = 0; k < dx.length; k++) {
+            let nx = x + dx[k];
+            let ny = y + dy[k];
+            if (
+              nx >= 0 &&
+              nx < board.length &&
+              ny >= 0 &&
+              board.length &&
+              board[nx][ny] === 1
+            ) {
+              board[nx][ny] = 0;
+              queue.push([nx, ny]);
+            }
+          }
+        }
       }
     }
   }
-
   return answer;
 }
 
-console.log(solution(8, 3));
+let arr = [
+  [1, 1, 0, 0, 0, 1, 0],
+  [0, 1, 1, 0, 1, 1, 0],
+  [0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 1, 1],
+  [1, 1, 0, 1, 1, 0, 0],
+  [1, 0, 0, 0, 1, 0, 0],
+  [1, 0, 1, 0, 1, 0, 0],
+];
+
+console.log(solution(arr));
