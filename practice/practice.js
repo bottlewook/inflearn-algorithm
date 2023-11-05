d33f65a756efbd931f123afe102aedc79a1b953b
wookki <bwlimtony@gmail.com>
2023-11-05 09:46:12 +0900

    feature/section-9/섬나라아일랜드-DFS

--- practice/practice.js
+++ practice/practice.js
@@ -1,35 +1,30 @@
 function solution(board) {
   let answer = 0;
+  let n = board.length;
   let dx = [-1, -1, 0, 1, 1, 1, 0, -1];
   let dy = [0, 1, 1, 1, 0, -1, -1, -1];
-  let queue = [];
 
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board.length; j++) {
-      if (board[i][j] === 1) {
-        board[i][j] = 0;
-        queue.push([i, j]);
-        answer++;
-        while (queue.length) {
-          let [x, y] = queue.shift();
-          for (let k = 0; k < dx.length; k++) {
-            let nx = x + dx[k];
-            let ny = y + dy[k];
-            if (
-              nx >= 0 &&
-              nx < board.length &&
-              ny >= 0 &&
-              board.length &&
-              board[nx][ny] === 1
-            ) {
-              board[nx][ny] = 0;
-              queue.push([nx, ny]);
-            }
-          }
-        }
+  function DFS(x, y) {
+    board[x][y] = 0;
+    for (let k = 0; k < dx.length; k++) {
+      let nx = x + dx[k];
+      let ny = y + dy[k];
+      if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] === 1) {
+        DFS(nx, ny);
       }
     }
   }
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (board[i][j] === 1) {
+        board[i][j] = 0;
+        answer++;
+        DFS(i, j);
+      }
+    }
+  }
+
   return answer;
 }
 
