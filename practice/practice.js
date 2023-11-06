89f06c53d8dae38e8a154613831d51a7560c67d5
wookki <bwlimtony@gmail.com>
2023-11-06 11:15:26 +0900

    feature/section-7/마굿간

--- practice/practice.js
+++ practice/practice.js
@@ -1,31 +1,31 @@
-function count(songs, capacity) {
+function count(stable, dist) {
   let count = 1;
-  let sum = 0;
-  for (let x of songs) {
-    if (sum + x > capacity) {
+  let position = stable[0];
+  for (let i = 1; i < stable.length; i++) {
+    if (stable[i] - position >= dist) {
       count++;
-      sum = x;
-    } else sum += x;
+      position = stable[i];
+    }
   }
   return count;
 }
-
-function solution(m, songs) {
-  let answer = 0;
-  let lt = 0;
-  let rt = songs.reduce((a, b) => a + b, 0);
+function solution(c, stable) {
+  let answer;
+  stable.sort((a, b) => a - b);
+  let lt = 1;
+  let rt = stable[stable.length - 1];
 
   while (lt <= rt) {
     let mid = Math.floor((lt + rt) / 2);
-    if (count(songs, mid) <= m) {
+    if (count(stable, mid) >= c) {
       answer = mid;
-      rt = mid - 1;
-    } else {
       lt = mid + 1;
+    } else {
+      rt = mid - 1;
     }
   }
   return answer;
 }
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let arr = [1, 2, 8, 4, 9];
 console.log(solution(3, arr));
